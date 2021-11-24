f240a02a6c882010839cc7894664502e95b337c9
Akhil <akhilaru339@gmail.com>
2021-11-24 20:27:03 +0530

    First commit

---
+++ try.js
@@ -0,0 +1,13 @@
+function func()
+{
+    var name = document.getElementById("name").value;
+    var pass = document.getElementById("pass").value;
+    if(name=="Akhil" && pass==1644)
+    {
+        alert("Successfull");
+    }
+    else
+    {
+        alert("Incorrect Credentials");
+    }
+}
